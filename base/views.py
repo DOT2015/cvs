6dd271f275797732094dc4415f4fd462040cb931
DOT2015 <djimayowa@yahoo.com>
2020-05-14 22:55:29 +0200

    commit

--- base/views.py
+++ base/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
-def home(request):
-    return render(request, 'base/home.html')
+from .models import Member
+
+def export(request):
+    response = HttpResponse(content_type='text/csv')
+
+    writer = csv.writer(response)
+    writer.writerow(['First Name', 'Last Name', 'Email', 'IP Address'])
+
+    for member in Member.objects.all().values_list('first_name', 'last_name', 'email', 'ip_address'):
+        writer.writerow(member)
+
+    response['Content-Disposition'] = 'attachment; filename="members.csv"'
+
+    return response
